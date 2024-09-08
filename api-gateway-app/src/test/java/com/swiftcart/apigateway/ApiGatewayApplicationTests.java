0a6b1799d6b26a48a0a07257f7fb0ea0da191bf7
saphal4859 <saisaphal123@gmail.com>
2024-09-08 21:58:13 +0530

    Integrated logback and reformatted code

--- api-gateway-app/src/test/java/com/swiftcart/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway-app/src/test/java/com/swiftcart/apigateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.swiftcart.api_gateway;
+package com.swiftcart.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
